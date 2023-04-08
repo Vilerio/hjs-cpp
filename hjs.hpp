1e3ff26cff8d9057d4b44c18c3e78fd22df67e73
Vilerio <olivier@thff.fr>
2023-04-08 14:04:43 +0200

    Adding the beggining of configuration functions

--- hjs.hpp
+++ hjs.hpp
@@ -3,7 +3,19 @@
 #include <iostream>
 #include <string>
 
+bool already_init{
+    return false;
+};
+
 void hjs_init(){
-    std::cout << "Initialisation de HJS..."
+    if (already_init){
+        std::cout << "HJS is already configured." << std::endl;
+    }
+    else{
+        std::cout << "Initialization of HJS..." << std::endl;
+        std::string directory;
+        std::cout << "Where do you want to install HJS ?" << "\n : ";
+        std::cin directory;
+    }
 }
 #endif HJS_HJS_H
