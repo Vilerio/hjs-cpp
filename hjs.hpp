9f2cdddb584589e8742a2c0b6fbbdcfe34929074
Vilerio <olivier@thff.fr>
2023-04-09 21:57:47 +0200

    Adding the main code of the functions hjs_init and already_init.

--- hjs.hpp
+++ hjs.hpp
@@ -2,9 +2,16 @@
 #define HJS_HJS_H
 #include <iostream>
 #include <string>
+#include "json.hpp"
 
 bool already_init{
-    return false;
+    // Open conf.json
+    std::ifstream json_mainconf_file("conf.json");
+    nlohmann::json json_mainconf_file_values;
+    json_mainconf_file >> json_mainconf_file_values;
+    json_mainconf_file.close();
+    json_mainconf_file_values["already_configured"] = already_configured_bool;
+    return already_configured_bool;
 };
 
 void hjs_init(std::string directory){
@@ -12,10 +19,17 @@ void hjs_init(std::string directory){
         std::cout << "HJS is already configured." << std::endl;
     }
     else{
-        std::cout << "Initialization of HJS..." << std::endl;
-        std::string directory;
-        std::cout << "Where do you want to install HJS ?" << "\n : ";
-        std::cin directory;
+        // Open conf.json
+        std::ifstream json_mainconf_file("conf.json");
+        nlohmann::json json_mainconf_file_values;
+        json_mainconf_file >> json_mainconf_file_values;
+        json_mainconf_file.close();
+        json_mainconf_file_values["storage_directory"] = actual_storage_directory;
+        // Open conf.json in reading mode
+        std::ofstream json_mainconf_file_write("conf.json");
+        json_mainconf_file_write << json_mainconf_file_values;
+        json_mainconf_file_write.close();
+        std::cout << "HJS is now configured." << std::endl;
     }
 }
 #endif HJS_HJS_H
